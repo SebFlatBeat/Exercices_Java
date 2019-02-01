fd1436b2e390191cfd13b959ae6496d6fac036d4
SebFlatBeat <sebastien.darre@gmail.com>
2019-02-01 15:00:45 +0100

    Creation de la fonction inversant la chaine

---
+++ Exercice 3/src/InvertChain.java
@@ -0,0 +1,10 @@
+public class InvertChain {
+
+    private String initial = "Java Inteview";
+    private StringBuffer stringBuffer = (new StringBuffer(initial)).reverse();
+
+    protected StringBuffer getStringBuffer() {
+        return stringBuffer;
+    }
+
+}

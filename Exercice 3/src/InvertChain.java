091a9e93203026f98511a4f6bd3a73c9fc745484
SebFlatBeat <sebastien.darre@gmail.com>
2019-02-04 21:17:09 +0100

    Revue complet du code de l'exercice 3

--- Exercice 3/src/InvertChain.java
+++ Exercice 3/src/InvertChain.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class InvertChain {
 
-    private String initial = "Java Interview";
-    private StringBuffer stringBuffer = (new StringBuffer(initial)).reverse();
-
-    protected StringBuffer getStringBuffer() {
-        return stringBuffer;
+    public static void main(String[] args) {
+        char reponse = 'O';
+        while (reponse == 'O') {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ecrivez un mot : ");
+            String initial = sc.nextLine();
+            StringBuffer stringBuffer = (new StringBuffer(initial)).reverse();
+            System.out.println(stringBuffer);
+            reponse = ' ';
+            while (reponse != 'O' && reponse != 'N') {
+                System.out.println("Voulez-vous réessayez ? (O/N)");
+                reponse = sc.next().charAt(0);
+            }
+        }
+        System.out.println("Merci, au revoir !");
     }
-
 }

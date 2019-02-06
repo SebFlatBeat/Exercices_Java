bf79af103f8338c11dcc1a907157e9e612abdc59
SebFlatBeat <sebastien.darre@gmail.com>
2019-02-06 11:29:55 +0100

    Ajout du Main dans exercice 4

---
+++ Exercice 4/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+      SearchHighNumber searchHighNumber = new SearchHighNumber();
+        System.out.println("Le plus grand nombre est : " + searchHighNumber.tab.length);
+    }
+}
--- Exercice 4/src/SearchHighNumber.java
+++ Exercice 4/src/SearchHighNumber.java
@@ -1,11 +1,9 @@
 public class SearchHighNumber {
 
-   int index;
+    int [] tab = {0,1,2,3,4,5,6,7,8,9};
     {
-        int[] tab = new int[50];
-        {
-            for (int index = 0; index < tab.length; index++) ;
-            System.out.println(tab[index]);
-        }
+        for (int index = 0; index < tab.length; index++);
     }
+
+
 }

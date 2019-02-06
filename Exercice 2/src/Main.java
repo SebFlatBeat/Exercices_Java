8c998f35db11c120544335db450fac88ee0adddf
SebFlatBeat <sebastien.darre@gmail.com>
2019-02-06 09:43:47 +0100

    Code Main de l'exercice 2 finit

--- Exercice 2/src/Main.java
+++ Exercice 2/src/Main.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String [] args) {
-        int[] tab = new int[8];
+    public static void main(String[] args) {
+        int[] tab = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        char reponse = 'O';
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entre un nombre : ");
-        int val = sc.nextInt();
-        Tableau tableau = new Tableau();
-        System.out.println(tableau.occurence(tab,val));
+        while (reponse == 'O') {
+            System.out.println("Entre un nombre : ");
+            int val = sc.nextInt();
+            Tableau tableau = new Tableau();
+            System.out.println(tableau.occurence(tab, val));
+            reponse = ' ';
+            while (reponse != 'O' && reponse != 'N') {
+                System.out.println("Voulez-vous réessayez ? (O/N)");
+                reponse = sc.next().charAt(0);
+            }
+        }
+        System.out.println("Merci, au revoir !");
     }
 }

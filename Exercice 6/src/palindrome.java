38c57407cbc6813d6232fd824a31b3186da1ba28
SebFlatBeat <sebastien.darre@gmail.com>
2019-02-09 10:50:43 +0100

    Réalisation de la fonction palindrome

---
+++ Exercice 6/src/palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class palindrome {
+
+    public static void main(String args[])
+    {
+        String original, reverse = "";
+        char reponse = 'O';
+        Scanner in = new Scanner(System.in);
+        while (reponse == 'O') {
+        System.out.println("Entrer un mot et nous allons vérifier si celui-ci est un palindrome : ");
+        original = in.next();
+
+        int length = original.length();
+
+        for (int i = length - 1; i >= 0; i--)
+            reverse = reverse + original.charAt(i);
+
+        if (original.equals(reverse))
+            System.out.println("C'est un palindrome.");
+        else
+            System.out.println("Ce n'est pas un palindrome.");
+            reponse = ' ';
+            while (reponse != 'O' && reponse != 'N') {
+                System.out.println("Voulez-vous réessayez ? (O/N)");
+                reponse = in.next().charAt(0);
+            }
+        }
+        System.out.println("Merci, au revoir !");
+    }
+}

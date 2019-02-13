daa8c5e6826f894fca10157faa87b6e73a748634
SebFlatBeat <sebastien.darre@gmail.com>
2019-02-13 11:25:39 +0100

    Stringtokenizer is deprecated Now use Split method

--- Exercice 7/src/Main.java
+++ Exercice 7/src/Main.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         String original;
         Scanner in = new Scanner(System.in);
         System.out.println(" Entrez votre phrase et nous verrons quel mot est le plus long : ");
         original = in.nextLine();
-        StringTokenizer stringTokenizer = new StringTokenizer(original);
-        System.out.println(stringTokenizer.countTokens());
+        String[] word = original.split(" ");
+        String bigWord = "";
+        for (int i = 0; i < word.length; i++) {
+            if (word[i].length() >= bigWord.length()) {
+                bigWord = word[i];
+            }
+        }
+        System.out.println(bigWord);
     }
 }
+
